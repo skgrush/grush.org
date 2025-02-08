958545b7c844c34e7717ab9463cc920e2d7beffb
Samuel K. Grush <Samuel@Grush.org>
2025-02-07 22:36:54 -0600

    repository component

---
+++ src/app/repository/github.service.ts
@@ -0,0 +1,72 @@
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { map, of, shareReplay, switchMap, timer } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { isPlatformServer } from '@angular/common';
+
+export type IGitHubRepo = {
+  id: number;
+  name: string;
+  full_name: string;
+  description: string | null;
+  fork: boolean;
+  html_url: string;
+  created_at: string;
+  pushed_at: string;
+  language: string;
+  archived: boolean;
+  disabled: boolean;
+  license: null | {
+    key: string;
+    name: string;
+    spdx_id: string;
+  };
+  owner: {
+    login: string;
+    avatar_url: string;
+    url: string;
+    html_url: string;
+  }
+  topics: string[];
+  watchers: string;
+  stars: number;
+  forks: number;
+  default_branch: string;
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  readonly #isServer = isPlatformServer(inject(PLATFORM_ID));
+
+  readonly githubResults$ = timer(1.5e3).pipe(
+    switchMap(() =>
+      this.#isServer
+        ? of(undefined)
+        : fromFetch('https://api.github.com/users/skgrush/repos', {
+            mode: 'cors',
+            priority: 'low',
+            cache: 'force-cache',
+            headers: {
+              'User-Agent': 'skgrush/grush.org',
+            },
+            selector: response => response.json() as Promise<Array<IGitHubRepo>>,
+          })
+    ),
+    map(repos =>
+      repos
+        ? new Map(repos.map(r => [r.name, r]))
+        : undefined
+    ),
+    shareReplay(1),
+    takeUntilDestroyed(),
+  );
+
+  getRepoByName$(name: string) {
+    return this.githubResults$.pipe(
+      map(repos => repos?.get(name)),
+    );
+  }
+}
---
+++ src/app/repository/repository.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, inject, input, ViewEncapsulation } from '@angular/core';
+import { GithubService, IGitHubRepo } from './github.service';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { Observable, switchMap } from 'rxjs';
+import { AsyncPipe, DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'grush-repository',
+  imports: [
+    AsyncPipe,
+    DatePipe,
+  ],
+  templateUrl: './repository.component.html',
+  styleUrl: './repository.component.scss',
+  encapsulation: ViewEncapsulation.ShadowDom,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RepositoryComponent {
+
+  readonly #githubService = inject(GithubService);
+
+  readonly repoName = input.required<string>();
+
+  readonly repoInfo$: Observable<IGitHubRepo | undefined> = toObservable(this.repoName).pipe(
+    switchMap(repoName => this.#githubService.getRepoByName$(repoName)),
+  )
+}
