1e7b661c72325d0e1af5d7f748c7a16a6713917c
Samuel K. Grush <Samuel@Grush.org>
2025-02-15 19:32:06 -0600

    wtiii

--- src/app/pages/app/gup/gup.component.ts
+++ src/app/pages/app/gup/gup.component.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RepositoryComponent } from '../../../repository/repository.component';
-import { TodoComponent } from '../../../todo/todo.component';
 
 @Component({
   selector: 'grush-gup',
   imports: [
     RepositoryComponent,
-    TodoComponent,
   ],
   templateUrl: './gup.component.html',
   styleUrl: './gup.component.scss',
