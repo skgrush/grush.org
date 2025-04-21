9ed62bc8e836887144a67f205be85c568c318664
Samuel K. Grush <Samuel@Grush.org>
2025-04-20 23:11:35 -0500

    add metadata

--- src/app/pages/Silly/routes.ts
+++ src/app/pages/Silly/routes.ts
@@ -4,6 +4,10 @@ export const routes = [
   {
     path: 'DateReducer',
     loadComponent: () => import('./date-reducer/date-reducer.component'),
+    title: 'Date reducer',
+    data: {
+      description: 'Reduce a date as if it were a ratio'
+    }
   }
 ] as const satisfies Routes;
 
